a034078ea5ebb1c863ba86cb076dcd17cc6a2e93
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-21 01:19:45 +0300

    add manager color

---
+++ lib/core/resources/manager_colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ManagerColors {
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color pink = Color(0xffDB3069);
+  static const Color orange = Color(0xffF99F00);
+  static const Color textColor = Colors.white;
+  static const Color buttonBorderColor = Colors.white;
+  static const Color pinkShadowColor = Color(0xffDB3167);
+  static const Color yellowShadowColor = Color(0xffF5D547);
+
+  static const Color darkBlueShadowColor = Color(0xff142246);
+  static const Color lightBlueShadowColor = Color(0xff345CC5);
+
+  static const Color moviesColor = Colors.black;
+
+  static const Color textCatColor = Color(0xFF666666);
+  static const Color movieTitleColor = Colors.black;
+
+  static const Color iconButtonNavigationBar = Color(0xff999999);
+  static const Color moreButtonColor = Color(0xFFFECB2F);
+
+  static const Color blueProfile = Color(0xFF446BCF);
+  static const Color numberLikes = Color(0xFFD6182A);
+
+  static const Color background = Color(0xFFF8F8F8);
+}
