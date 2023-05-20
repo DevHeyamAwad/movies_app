e5818fcccc65413e3a1ec300adafebce42c516c5
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-21 01:27:48 +0300

    add manager fonts

---
+++ lib/core/resources/manager_fonts.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../util/size_util.dart';
+
+class ManagerFontFamily {
+  static const String fontFamily = "Poppins";
+}
+
+class ManagerFontWeight {
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class ManagerFontSize {
+  static double s2 = SizeUtil.setFontSize(2.0);
+  static double s4 = SizeUtil.setFontSize(4.0);
+  static double s6 = SizeUtil.setFontSize(6.0);
+  static double s8 = SizeUtil.setFontSize(8.0);
+  static double s10 = SizeUtil.setFontSize(10.0);
+  static double s11 = SizeUtil.setFontSize(11.0);
+  static double s12 = SizeUtil.setFontSize(12.0);
+  static double s14 = SizeUtil.setFontSize(14.0);
+  static double s15 = SizeUtil.setFontSize(15.0);
+  static double s16 = SizeUtil.setFontSize(16.0);
+  static double s18 = SizeUtil.setFontSize(18.0);
+  static double s20 = SizeUtil.setFontSize(20.0);
+  static double s22 = SizeUtil.setFontSize(22.0);
+  static double s24 = SizeUtil.setFontSize(24.0);
+  static double s26 = SizeUtil.setFontSize(26.0);
+  static double s28 = SizeUtil.setFontSize(28.0);
+  static double s30 = SizeUtil.setFontSize(30.0);
+  static double s35 = SizeUtil.setFontSize(35.0);
+  static double s40 = SizeUtil.setFontSize(40.0);
+  static double s80 = SizeUtil.setFontSize(80.0);
+  static double s100 = SizeUtil.setFontSize(100.0);
+}
