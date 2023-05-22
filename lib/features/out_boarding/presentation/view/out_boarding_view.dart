89341a309cbb8ff0d099e50263874dcb633e252e
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-23 02:50:30 +0300

    add out boarding view

---
+++ lib/features/out_boarding/presentation/view/out_boarding_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OutBoardingView extends StatelessWidget {
+  const OutBoardingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
