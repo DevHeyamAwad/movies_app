c0e2598db1362f60ffb5bcb9e6094fdd5e42a709
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-23 02:07:42 +0300

    add dependancy injection splash

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,3 +1,7 @@
 class Constans {
   static const String kLinkeProject = '';
+  static const double deviceWidth = 375;
+  static const double deviceHeight = 812;
+  static const int splashDuration = 3;
+  static const double elevationAppBar = 0;
 }
--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -1,6 +1,18 @@
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
 import 'package:get_it/get_it.dart';
 
+import '../features/splash/presentation/controller/splash_controller.dart';
+
 final instance = GetIt.instance;
-initModule() {}
-//
-//
+initSplash() {
+  Get.put<SplashController>(SplashController());
+}
+
+finishSplash() {
+  Get.delete<SplashController>();
+}
+
+initOutBoarding() {
+  finishSplash();
+}
