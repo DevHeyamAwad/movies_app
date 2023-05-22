ec35af22cb9f22299d4650bf8de60073ae703c78
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-23 02:26:54 +0300

    add manager light theme

--- lib/core/theme/manager_light_theme.dart
+++ lib/core/theme/manager_light_theme.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
+import '../../config/constants.dart';
 import '../resources/manager_colors.dart';
+import '../resources/manager_fonts.dart';
+import '../resources/manager_styles.dart';
 
 ThemeData managerLightThemeData() {
   return ThemeData.light().copyWith(
     useMaterial3: true,
-    primaryColor: ManagerColors.darkBlueShadowColor,
+
+    /// MAIN COLORS APP
+    primaryColor: ManagerColors.black,
+    primaryColorLight: ManagerColors.white,
+
+    // this for disabled button colors
+    disabledColor: ManagerColors.buttonBorderColor,
+    scaffoldBackgroundColor: ManagerColors.background,
+
+    /// APP BAR THEME
+    appBarTheme: AppBarTheme(
+      color: ManagerColors.white,
+      elevation: Constants.elevationAppBar,
+      titleTextStyle: getBoldTextStyle(
+        fontSize: ManagerFontSize.s14,
+        color: ManagerColors.black,
+      ),
+    ),
+
+    /// BUTTON THEME
+    buttonTheme: const ButtonThemeData(
+      shape: RoundedRectangleBorder(),
+      disabledColor: ManagerColors.buttonBorderColor,
+    ),
+
+    textTheme: ManagerTextThemeLight(),
   );
 }
