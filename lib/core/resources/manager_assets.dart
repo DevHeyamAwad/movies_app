2fb6a6851bb5836d4094c5d9d7b48fffb4c7774e
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-28 00:27:15 +0300

    edit pageviewitems & add outboarding background to managerassets

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -4,7 +4,13 @@ class ManagerAssets {
   static const String logo = '$imagePath/logo.png';
   static const String background = '$imagePath/background.png';
   static const String backgroundLogo = '$imagePath/background-logo.png';
-  static const String outBoardingImage1 = '$imagePath/out-boarding-1.png';
-  static const String outBoardingImage2 = '$imagePath/out-boarding-2.png';
-  static const String outBoardingImage3 = '$imagePath/out-boarding-3.png';
+  static const String outBoardingImage1 = '$imagePath/out-boarding-image-1.png';
+  static const String outBoardingImage2 = '$imagePath/out-boarding-image-2.png';
+  static const String outBoardingImage3 = '$imagePath/out-boarding-image-3.png';
+  static const String outBoardingBackground1 =
+      '$imagePath/out-boarding-background-1.png';
+  static const String outBoardingBackground2 =
+      '$imagePath/out-boarding-background-2.png';
+  static const String outBoardingBackground3 =
+      '$imagePath/out-boarding-background-3.png';
 }
--- lib/features/out_boarding/presentation/controller/out_boarding_controller.dart
+++ lib/features/out_boarding/presentation/controller/out_boarding_controller.dart
@@ -2,21 +2,31 @@ import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:movies_app/core/resources/manager_assets.dart';
 import 'package:movies_app/core/resources/manager_strings.dart';
 
+import '../../../../core/widegts/scaffold_with_background_image.dart';
 import '../view/widgets/out_boarding_item.dart';
 
 class OutBoardingController extends GetxController {
   final List pageViewItems = [
-    OutBoardingItem(
-      image: ManagerAssets.outBoardingImage1,
-      text: ManagerStrings.outBoardingTitle1,
+    scaffoldWithBackGroundImage(
+      image: ManagerAssets.outBoardingBackground1,
+      child: OutBoardingItem(
+        image: ManagerAssets.outBoardingImage1,
+        text: ManagerStrings.outBoardingTitle1,
+      ),
     ),
-    OutBoardingItem(
-      image: ManagerAssets.outBoardingImage2,
-      text: ManagerStrings.outBoardingTitle2,
+    scaffoldWithBackGroundImage(
+      image: ManagerAssets.outBoardingBackground2,
+      child: OutBoardingItem(
+        image: ManagerAssets.outBoardingImage2,
+        text: ManagerStrings.outBoardingTitle2,
+      ),
     ),
-    OutBoardingItem(
-      image: ManagerAssets.outBoardingImage3,
-      text: ManagerStrings.outBoardingTitle3,
+    scaffoldWithBackGroundImage(
+      image: ManagerAssets.outBoardingBackground3,
+      child: OutBoardingItem(
+        image: ManagerAssets.outBoardingImage3,
+        text: ManagerStrings.outBoardingTitle3,
+      ),
     ),
   ];
 }
