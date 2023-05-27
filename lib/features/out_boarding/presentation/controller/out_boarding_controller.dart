86a70a216c571f26d321f9143103d1f9563bdbf8
Heyam Awad <heyamtomaawad@gmail.com>
2023-05-28 02:27:58 +0300

    create setCurrentPage & add sharedprefs to dependancyinjection file

--- lib/features/out_boarding/presentation/controller/out_boarding_controller.dart
+++ lib/features/out_boarding/presentation/controller/out_boarding_controller.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:movies_app/config/constants.dart';
+import 'package:movies_app/config/dependency_injection.dart';
 import 'package:movies_app/core/resources/manager_assets.dart';
 import 'package:movies_app/core/resources/manager_strings.dart';
+import 'package:movies_app/core/storage/local/app_settings_shared_preferances.dart';
 
 import '../../../../core/widegts/scaffold_with_background_image.dart';
 import '../view/widgets/out_boarding_item.dart';
@@ -12,6 +14,8 @@ class OutBoardingController extends GetxController {
   static const firstPage = 0;
   static const lastPage = 2;
   int currentPage = firstPage;
+final AppSettingsSharedPreferences _appSettingsSharedPreferences = instance<AppSettingsSharedPreferences>();
+
   final List pageViewItems = [
     scaffoldWithBackGroundImage(
       image: ManagerAssets.outBoardingBackground1,
@@ -78,5 +82,9 @@ class OutBoardingController extends GetxController {
       animatedToPage(index: --currentPage);
       update();
     }
+  }
+
+  Future<void> setCurrentPage() async {
+    await 
   }
 }
