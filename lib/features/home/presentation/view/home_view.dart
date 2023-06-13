b39c94ad177e901efd34ed7ad20e79f0f9ae93f4
Heyam Awad <heyamtomaawad@gmail.com>
2023-06-13 06:24:18 +0300

    create home view file

---
+++ lib/features/home/presentation/view/home_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
