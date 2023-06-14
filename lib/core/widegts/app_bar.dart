d8dced204c3d944122eb02bf72f1cefb4624f526
Heyam Awad <heyamtomaawad@gmail.com>
2023-06-14 06:02:03 +0300

    create custom appbar

---
+++ lib/core/widegts/app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/config/constants.dart';
+import 'package:movies_app/core/extensions/extensions.dart';
+import 'package:movies_app/core/resources/manager_colors.dart';
+import 'package:movies_app/core/resources/manager_sizes.dart';
+import 'package:movies_app/core/resources/manager_styles.dart';
+
+AppBar mainAppBar({IconData? icon, required String? appBarText}) {
+  return AppBar(
+    title: Text(
+      appBarText.onNull(),
+      style: getRegularStyle(
+        color: ManagerColors.black,
+        fontSize: ManagerSize.s24,
+      ),
+    ),
+    actions: [
+      Padding(
+        padding: const EdgeInsets.only(
+          right: Constants.paddingIconAppBar,
+        ),
+        child: Icon(
+          icon,
+          size: ManagerIconSize.s25,
+        ),
+      )
+    ],
+  );
+}
