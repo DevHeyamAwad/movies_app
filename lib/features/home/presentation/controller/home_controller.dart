ecac1ff7ebd014e13cbeff49986b9d8ccd1d62fb
Heyam Awad <heyamtomaawad@gmail.com>
2023-06-14 06:01:49 +0300

    create chane method

--- lib/features/home/presentation/controller/home_controller.dart
+++ lib/features/home/presentation/controller/home_controller.dart
@@ -1,3 +1,19 @@
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:carousel_slider/carousel_controller.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  late CarouselController carouselController;
+  int selectedCategoryIndex = 0;
+  int current = 0;
+  @override
+  void onInit() {
+    // TODO: implement onInit
+
+    carouselController = CarouselController();
+  }
+
+  void change(index) {
+    current = index;
+    update();
+  }
+}
