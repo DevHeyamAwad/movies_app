7d14773340aa46768b194007d0f8c30c6d300c57
Heyam Awad <heyamtomaawad@gmail.com>
2023-06-14 06:03:13 +0300

    add CarouselSlider to home view SIP

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,11 +1,31 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:movies_app/core/resources/manager_strings.dart';
+import 'package:movies_app/core/widegts/will_pop_scope.dart';
+import 'package:movies_app/features/home/presentation/controller/home_controller.dart';
+
+import '../../../../core/widegts/app_bar.dart';
+import 'widgets/custom_banner.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return willPopScope(
+      child: Scaffold(
+        appBar: mainAppBar(
+          appBarText: ManagerStrings.movies,
+          icon: Icons.search,
+        ),
+        body: GetBuilder<HomeController>(builder: (controller) {
+          return ListView(
+            children: const [
+              CustomBanner(),
+            ],
+          );
+        }),
+      ),
+    );
   }
 }
