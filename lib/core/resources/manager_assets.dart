22fa6df18676bda1fe66aed565398259f72aaa5b
Heyam Awad <heyamtomaawad@gmail.com>
2023-06-14 06:00:40 +0300

    add assets to manager

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -3,6 +3,10 @@ const String imagePath = 'assets/images';
 class ManagerAssets {
   static const String logo = '$imagePath/logo.png';
   static const String background = '$imagePath/background.png';
+  static const String tv = '$imagePath/tv.png';
+  static const String profile = '$imagePath/profile.png';
+  static const String carousel1 = '$imagePath/carousel1.png';
+  static const String carousel2 = '$imagePath/carousel2.png';
   static const String backgroundLogo = '$imagePath/background-logo.png';
   static const String outBoardingImage1 = '$imagePath/out-boarding-image-1.png';
   static const String outBoardingImage2 = '$imagePath/out-boarding-image-2.png';
