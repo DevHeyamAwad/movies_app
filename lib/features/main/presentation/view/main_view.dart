4247887fb88b9fb13164a309553a7d059d30801a
Heyam Awad <heyamtomaawad@gmail.com>
2023-06-14 06:06:16 +0300

    finish main view & add dependancies to yaml

--- lib/features/main/presentation/view/main_view.dart
+++ lib/features/main/presentation/view/main_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:movies_app/core/resources/manager_colors.dart';
 import 'package:movies_app/core/resources/manager_sizes.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 
@@ -13,22 +14,14 @@ class MainView extends StatelessWidget {
     return GetBuilder<MainController>(
       builder: (controller) {
         return PersistentTabView(
+          decoration: const NavBarDecoration(
+              boxShadow: [BoxShadow(color: ManagerColors.black)]),
           context,
           controller: controller.persistentTabController,
           backgroundColor: Colors.white,
           navBarStyle: NavBarStyle.style2,
           confineInSafeArea: true,
-          navBarHeight: ManagerHeight.h60,
-          decoration: NavBarDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(
-                ManagerRadius.r16,
-              ),
-              topRight: Radius.circular(
-                ManagerRadius.r16,
-              ),
-            ),
-          ),
+          navBarHeight: ManagerHeight.h65,
           screens: controller.screens,
           items: controller.bottomNavBarItems,
         );
