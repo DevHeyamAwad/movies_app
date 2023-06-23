5ff43efe0befac376bd2b1d09f78602672de3ea9
Heyam Awad <heyamtomaawad@gmail.com>
2023-06-23 03:26:29 +0300

    add constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Constants {
   static const String kLinkeProject = '';
   static const double deviceWidth = 375;
@@ -8,7 +10,20 @@ class Constants {
   static const double elevationButton = 0;
   static const int sliderItems = 3;
   static const double paddingIconAppBar = 16;
-  static const double viewportFraction = 0.85;
+  static const double mainViewportFraction = 0.85;
+  static const double catViewportFraction = 0.4;
+  static const double popViewportFraction = 0.4;
+  static const int maxLines = 2;
+  static const Offset offset = Offset(
+    0,
+    4,
+  );
+  static const double catBlurRadius = 8;
+  static const Offset mainOffset = Offset(
+    0,
+    4,
+  );
+  static const double mainBlurRadius = 3;
 }
 
 class ConstantsPrefsKeys {
