d41a8301d3513cc3df0c0a24a303e4fe4e09583f
Heyam Awad <heyamtomaawad@gmail.com>
2023-07-06 03:18:20 +0300

    create extentions

--- lib/core/extensions/extensions.dart
+++ lib/core/extensions/extensions.dart
@@ -12,6 +12,26 @@ extension NonNullString on String? {
   }
 }
 
+extension NonNullInt on int? {
+  int onNull() {
+    if (this == null) {
+      return 0;
+    } else {
+      return this!;
+    }
+  }
+}
+
+extension NonNullListOfInt on List<int>? {
+  List<int> onNull() {
+    if (this == null) {
+      return [];
+    } else {
+      return this!;
+    }
+  }
+}
+
 extension NonNulldouble on double? {
   double onNull() {
     if (this == null) {
