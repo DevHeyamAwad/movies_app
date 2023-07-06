bf229122e27901a1c17156bc8c1bfb6957243151
Heyam Awad <heyamtomaawad@gmail.com>
2023-07-06 03:21:10 +0300

    movie response g

--- lib/features/home/data/response/movie_response.g.dart
+++ lib/features/home/data/response/movie_response.g.dart
@@ -9,34 +9,35 @@ part of 'movie_response.dart';
 MovieDetailResponse _$MovieDetailResponseFromJson(Map<String, dynamic> json) =>
     MovieDetailResponse(
       adult: json['adult'] as bool?,
-      backdropPath: json['backdrop_path'] as String?,
+      backdropPath: json['backdropPath'] as String?,
       budget: json['budget'] as int?,
-      genresResponse: json['genres'] == null
+      genresResponse: json['genresResponse'] == null
           ? null
-          : GenresResponse.fromJson(json['genres'] as Map<String, dynamic>),
+          : GenresResponse.fromJson(
+              json['genresResponse'] as Map<String, dynamic>),
       homepage: json['homepage'] as String?,
       id: json['id'] as int?,
-      imdbId: json['imdb_id'] as String?,
-      originalLanguage: json['original_language'] as String?,
-      originalTitle: json['original_title'] as String?,
+      imdbId: json['imdbId'] as String?,
+      originalLanguage: json['originalLanguage'] as String?,
+      originalTitle: json['originalTitle'] as String?,
       overview: json['overview'] as String?,
       popularity: (json['popularity'] as num?)?.toDouble(),
-      posterPath: json['poster_path'] as String?,
+      posterPath: json['posterPath'] as String?,
     );
 
 Map<String, dynamic> _$MovieDetailResponseToJson(
         MovieDetailResponse instance) =>
     <String, dynamic>{
       'adult': instance.adult,
-      'backdrop_path': instance.backdropPath,
+      'backdropPath': instance.backdropPath,
       'budget': instance.budget,
-      'genres': instance.genresResponse,
+      'genresResponse': instance.genresResponse,
       'homepage': instance.homepage,
       'id': instance.id,
-      'imdb_id': instance.imdbId,
-      'original_language': instance.originalLanguage,
-      'original_title': instance.originalTitle,
+      'imdbId': instance.imdbId,
+      'originalLanguage': instance.originalLanguage,
+      'originalTitle': instance.originalTitle,
       'overview': instance.overview,
       'popularity': instance.popularity,
-      'poster_path': instance.posterPath,
+      'posterPath': instance.posterPath,
     };
