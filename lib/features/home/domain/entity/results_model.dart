e70b2713249aace53d2e05f789a514c5e7264df0
Heyam Awad <heyamtomaawad@gmail.com>
2023-07-06 03:22:14 +0300

    results model

---
+++ lib/features/home/domain/entity/results_model.dart
@@ -0,0 +1,32 @@
+class ResultsEntity {
+  bool? adult;
+  String? backdropPath;
+  List<int>? genreIds;
+  int? id;
+  String? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  String? releaseDate;
+  String? title;
+  bool? video;
+  double? voteAverage;
+  int? voteCount;
+  ResultsEntity({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+}
